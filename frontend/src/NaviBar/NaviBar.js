c0ed8de8c1924644be81a47985d40d911e2b4428
Oleg Shipulin <olegshipu95@gmail.com>
2022-12-13 00:59:07 +0300

    fix NavBar

--- frontend/src/NaviBar/NaviBar.js
+++ frontend/src/NaviBar/NaviBar.js
@@ -14,9 +14,9 @@ export default function NaviBar() {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-between">
                         <Nav className="mr-auto">
-                            <Nav.Link>Home</Nav.Link>
+                            <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link>Table</Nav.Link>
-                            <Nav.Link>About</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
                             <Nav.Link>Exercise</Nav.Link>
                         </Nav>
                         <Nav>
